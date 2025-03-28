b8f67f89083d42cff257b4dc5064221ca04ba0d1
fuadmn <fuadmohamednor@gmail.com>
2025-03-29 00:43:56 +0300

    Build Your First API

--- api.php
+++ api.php
@@ -8,6 +8,39 @@ header("Content-type: application/json");
 //function Update
 
 
+function readAll($conn){
 
+    $data = array();
+    $message = array();
+    // read All students in the database
+    $query = "SELECT * FORM student";
+
+    // excute the query
+
+    $result = $conn->($query);
+
+    // success or error
+    if($result){
+
+        while($row = $result->fetch_assoc()){
+
+            $data [] = $row;
+
+
+        }
+
+        $message = array("status" => true, "data" => $data);
+
+    }else{
+ 
+        $message = array("status" => false, "data" => $conn->error);
+
+    }
+
+
+    echo json_encode($message);
+
+
+}
 
 ?>
