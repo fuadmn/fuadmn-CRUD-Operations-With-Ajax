f796252dea145ac57f404713b35fc4d32865887b
fuadmn <fuadmohamednor@gmail.com>
2025-03-28 14:33:40 +0300

    Design Our UI

--- index.php
+++ index.php
@@ -3,18 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jquery Basics</title>
+    <title>CRUD Operations</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
     
-      <h1 class="heading_1">Hello World</h1>
-      <h2 id="heading_2">Hello World</h2>
-      <h3>Hello World</h3>
-      
-      <button>Click Here</button>
+      <div class="container">
+
+      <div class="row justify-content-centre">
+
+      <div class="col-sm-8">
+
+      <h1 class="fs-3 text-center">Users Info</h1>
+
+      <table class="table">
+       
+          <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Class</th>
+                    <th>Action</th>
+                </tr>
+          </thead>
+      </table>
+
+      </div>
+
+      </div>
+      </div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-
-       <script src="main.js"></script>
 </body>
 </html>
