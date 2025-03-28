20d0f3796106c724850a3575e93c6bfbd3011b56
fuadmn <fuadmohamednor@gmail.com>
2025-03-28 13:53:31 +0300

    Jquery Basics

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jquery Basics</title>
+</head>
+<body>
+    
+      <h1 class="heading_1">Hello World</h1>
+      <h2 id="heading_2">Hello World</h2>
+      <h3>Hello World</h3>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+       <script src="main.js"></script>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,7 @@
+// . # tag
+
+$(".heading_1").html("This is the js document");
+
+$("#heading_2").css("backgroundColor","#765675");
+
+$("h3").css("font-size","50px")
