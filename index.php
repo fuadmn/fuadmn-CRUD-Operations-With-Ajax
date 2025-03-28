bf7fbc522e625a8a9a6dff214edd7c85290851c2
fuadmn <fuadmohamednor@gmail.com>
2025-03-28 14:14:40 +0300

    Jquery Events

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
       <h1 class="heading_1">Hello World</h1>
       <h2 id="heading_2">Hello World</h2>
       <h3>Hello World</h3>
+      
+      <button>Click Here</button>
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
--- main.js
+++ main.js
@@ -4,4 +4,10 @@ $(".heading_1").html("This is the js document");
 
 $("#heading_2").css("backgroundColor","#765675");
 
-$("h3").css("font-size","50px")
+$("h3").css("font-size","50px");
+
+// Events in jquery
+
+$("button").click(function(){
+    $("body").css("backgroundColor","#876575")
+})
